31382cd72ecfd65fe33a60944cb9d75b18535c68
Christopher Rupp <chris@techelevator.com>
2018-01-30 21:43:52 -0500

    expressions lecture

---
+++ m1-w1d3-expressions-lecture/Lecture/09_ReturnTrueWhenGreaterThanFiveInOneLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+         9. How can we rewrite exercise 8 to have only one line of code?
+            TOPIC: Boolean Expression & Comparison Operators
+        */
+        public bool ReturnTrueWhenGreaterThanFiveInOneLine(int number)
+        {
+            bool result = false;
+            // What can we put here that returns a bool that we want?
+            return result; 
+        }
+
+    }
+}
