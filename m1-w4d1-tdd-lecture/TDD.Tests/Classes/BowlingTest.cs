77aec13e897904e29a73f12c7b144e28d95fa559
Christopher Rupp <chris@techelevator.com>
2018-02-19 11:34:47 -0500

    tdd

---
+++ m1-w4d1-tdd-lecture/TDD.Tests/Classes/BowlingTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD.Classes;
+
+namespace TDD.Tests.Classes
+{
+    [TestClass]
+    public class BowlingTest
+    {
+        Bowling bowling = new Bowling();
+
+        [TestMethod]
+        public void Bowling_AllFramesGutterBall_ReturnsScore()
+        {
+            int[] balls = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int score = bowling.Scoring(balls);
+            Assert.AreEqual(0, score, "The score for all gutter balls should have been zero.");
+        }
+
+        [TestMethod]
+        public void Bowling_AllFramesOnePin_ReturnsScore()
+        {
+            int[] balls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
+            int score = bowling.Scoring(balls);
+            Assert.AreEqual(20, score, "The score for all 1 pin balls should have been 20.");
+        }
+
+        [TestMethod]
+        // This will test a spare in the first frame followed by 3 pins in the second
+        // frame and then all misses after that
+        public void Bowling_FirstFrameSpare_ReturnsScore()
+        {
+            int[] balls = { 5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            bowling.AddScore(5);
+            bowling.AddScore(5);
+            bowling.AddScore(3);
+            Assert.AreEqual(16, bowling.Score, "The score for a spare followed by 3 pins is 16.");
+        }
+
+        [TestMethod]
+        // This will test a strike in the first frame followed by 3 pins and then 4 pins in the second
+        // frame and then all misses after that
+        public void Bowling_FirstFrameStrike_ReturnsScore()
+        {
+            int[] balls = { 10, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int score = bowling.Scoring(balls);
+            Assert.AreEqual(24, score, "The score for a strike followed by 3 pins and then 4 is 24.");
+        }
+
+        [TestMethod]
+        public void Bowling_PerfectGame_ReturnsScore()
+        {
+            int[] balls = { 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10 };
+            int score = bowling.Scoring(balls);
+            Assert.AreEqual(300, score, "The score a perfect game is 300.");
+        }
+
+        //[TestMethod]
+        //public void ScoreTenthFrame_Turkey_ReturnsScore()
+        //{
+        //    int score = bowling.ScoreTenthFrame(10, 10, 10);
+        //    Assert.AreEqual(30, score, "The score is a turkey.");
+        //}
+
+        //[TestMethod]
+        //public void ScoreTenthFrame_Spare_ReturnsScore()
+        //{
+        //    int score = bowling.ScoreTenthFrame(5, 5, 7);
+        //    Assert.AreEqual(17, score, "The score is 17.");
+        //}
+
+        //[TestMethod]
+        //public void ScoreTenthFrame_NoSpareOrStrike_ReturnsScore()
+        //{
+        //    int score = bowling.ScoreTenthFrame(5, 4, 0);
+        //    Assert.AreEqual(9, score, "The score is 9.");
+        //}
+    }
+}
---
+++ m1-w4d1-tdd-lecture/TDD/Classes/Bowling.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Classes
+{
+    public class Bowling
+    {
+        const int MaxFrameScore = 10;
+        const int Strike = MaxFrameScore;
+
+        private int[] _balls = new int[21];
+        private int _ballCtr = 0;
+
+        public int Score
+        {
+            get
+            {
+                return Scoring(_balls);
+            }
+        }
+
+        public Bowling()
+        {
+        }
+
+        /// <summary>
+        /// Adds the pins knocked down to the score
+        /// </summary>
+        /// <param name="pinsKnockedDown">Number of pins knocked down</param>
+        public void AddScore(int pinsKnockedDown)
+        {
+            if (_ballCtr < 20)
+            {
+                _balls[_ballCtr] = pinsKnockedDown;
+                _ballCtr++;
+            }
+            else
+            {
+                throw new Exception("Game Over");
+            }
+        }
+
+        /// <summary>
+        /// Scores an entire game of bowling
+        /// </summary>
+        /// <param name="balls">Array of all the ball scores</param>
+        /// <returns>Returns the total score</returns>
+        public int Scoring(int[] balls)
+        {
+            int result = 0;
+            
+            // Scores all the balls for frames 1 - 9
+            for (int ballCtr = 0; ballCtr < 18; ballCtr += 2)
+            {
+                result += ScoreFrame(balls[ballCtr], 
+                                     balls[ballCtr + 1], 
+                                     balls[ballCtr + 2], 
+                                     balls[ballCtr + 3], 
+                                     balls[ballCtr + 4]);
+            }
+
+            // Scores the last 3 balls of the array for frame 10
+            result += ScoreTenthFrame(balls[18], balls[19], balls[20]);
+
+            return result;
+        }
+
+        private int ScoreFrame(int f1B1, int f1B2, int f2B1, int f2B2, int f3B1)
+        {
+            int result = f1B1 + f1B2;
+            bool isStrike = (f1B1 == Strike);
+            bool isSpare = ((f1B1 + f1B2) == MaxFrameScore) && !isStrike;
+
+            // If we are a strike in the first frame
+            if (isStrike)
+            {
+                if (f2B1 == Strike)
+                {
+                    result += (f2B1 + f3B1);
+                }
+                else
+                {
+                    result += (f2B1 + f2B2);
+                }
+            }
+            // If we are a spare in the first frame
+            else if (isSpare)
+            {
+                result += f2B1;
+            }
+
+            return result;
+        }
+
+        private int ScoreTenthFrame(int ball1, int ball2, int ball3)
+        {
+            int result = ball1 + ball2;
+
+            // If we are a strike on the first ball
+            if (ball1 == Strike || (ball1 + ball2 == MaxFrameScore))
+            {
+                result += ball3;
+            }
+
+            return result;
+        }
+    }
+}
