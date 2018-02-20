0cfcd1d6788eab84f1c84f29a59927208ae7cee2
Christopher Rupp <chris@techelevator.com>
2018-02-20 11:58:46 -0500

    file io

---
+++ m1-w4d2-file-io-part1-lecture/ExceptionHandling/Exceptions/OverdraftException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Exceptions
+{
+    public class OverdraftException : Exception
+    {
+        private double overdraftAmount = 0.0;
+        public double OverdraftAmount
+        {
+            get { return overdraftAmount; }  
+            set { overdraftAmount = value; }
+        }
+
+        public OverdraftException(string message, double overdraftAmount)
+            : base(message)
+        {
+            this.overdraftAmount = overdraftAmount;
+        }
+
+        public OverdraftException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ m1-w4d2-file-io-part1-lecture/Lecture/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lecture.Aids;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FilePaths.UsingPathToCombineTwoFilePaths();
+            //FilePaths.GettingExtensions();
+            //FilePaths.UsingPathForTemporaryFolders();
+
+            ReadingInFiles.ReadACharacterFile();
+            ReadingCSVFiles.ReadFile();
+            SummingUpNumbers.ReadFile(); 
+
+            Console.ReadKey();
+        }
+    }
+}
