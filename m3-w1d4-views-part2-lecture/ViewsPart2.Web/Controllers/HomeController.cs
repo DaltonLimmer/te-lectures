2edad72fcd3ce3279db4e2e6b07f1754d5708705
Aaron Tyler <aaron.tyler@techelevator.com>
2018-03-15 08:40:36 -0400

    Added m3-w1d4 Views Part 2 Lecture - Before Lecture

---
+++ m3-w1d4-views-part2-lecture/ViewsPart2.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewsPart2.Web.Models;
+
+namespace ViewsPart2.Web.Controllers
+{
+    /*
+    * Each Controller Action below responds to a request from a browser.
+    * If the user visits http://localhost/Home/Index then the
+    * Index Action will execute, returning the Index view.
+    */
+    public class HomeController : Controller
+    {
+
+        // GET: Home/Index
+        // GET: Home/
+        // GET: /
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Home/Colors
+        public ActionResult Colors()
+        {
+            return View();
+        }
+
+        // GET: Home/Team
+        public ActionResult Team()
+        {
+            return View();
+        }
+    }
+}
