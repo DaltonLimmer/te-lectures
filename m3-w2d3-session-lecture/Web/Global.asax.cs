0240ffebb865b8869a160270b30af24d55c28fa9
Aaron Tyler <aaron.tyler@techelevator.com>
2018-03-21 08:43:43 -0400

    Added m3-w2d3 session pre-lecture

---
+++ m3-w2d3-session-lecture/Web/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using Ninject.Web.Common.WebHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Web.DAL;
+
+namespace Web
+{
+    public class MvcApplication : NinjectHttpApplication
+    {
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            
+
+            return kernel;
+        }
+
+
+
+    }
+}
